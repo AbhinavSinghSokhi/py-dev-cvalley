6de495ad0d4d5a967abb2ef258964ce272383817
Abhinav Singh <sokhiabhinavsingh@gmail.com>
2024-04-27 23:49:17 +0530

    Login System and Tasks model added

--- campusvalley/todo_list/urls.py
+++ campusvalley/todo_list/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signup", views.signup, name="singupPage"),
+    path("loginform", views.loginForm, name="loginform"),
+    path("signupform", views.signupForm, name="signupform"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("new_task", views.new_task, name="new_task"),
+    path("logout", views.logout_user, name="logout")
 ]
--- campusvalley/todo_list/views.py
+++ campusvalley/todo_list/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User, Task
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def loginForm(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(dashboard)
+        else:
+            return render(request, 'index.html', {'error': 'Invalid Credentials'})
+    else:
+        return render(request, "index.html")
+
+
+def signupForm(request):
+    if request.method=="POST":
+        username= request.POST.get("username")
+        email= request.POST.get("email")
+        password= request.POST.get("password")
+        User.objects.create_user(username=username, email=email, password=password)
+        return redirect(index)
+    else:
+        return render(request, "signup.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html", {'user_details': request.user})
+
+
+def new_task(request):
+    if request.method=="POST":
+        task_title= request.POST.get("task_title")
+        task_desc= request.POST.get("task_desc")
+        task_category= request.POST.get("list")
+        task_due_date= request.POST.get("due_date")
+        # response= task_title + " " + task_desc+ " " + task_category + " "+ task_due_date
+        # return HttpResponse(response, status=200)
+        user= request.user
+        user_name= User.objects.get(username= user.username)
+        task= Task.objects.create(user= user_name, title=task_title, description=task_desc, category=task_category, dueDate=task_due_date )
+        return render(request, "dashboard.html",{'user_details': request.user} )
+    else:
+        return redirect(dashboard)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(index)
